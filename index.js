32b2905ae94536bc90ec9b36ab725e4d77fe2f52
Sajid <sajidmomin8340@gmail.com>
2024-06-15 14:31:33 +0530

    sajid

--- index.js
+++ index.js
@@ -1,32 +1,39 @@
-import express, { request, response } from 'express'
-import mongoose, { model } from 'mongoose';
-import { PORT, mongoDBURL } from './config.js';
-import itemsRoute  from './routes/itemsRoute.js';
-import companyrouter from './routes/companyrouter.js';
+import express from 'express';
+import mongoose from 'mongoose';
 import cors from 'cors';
+import { PORT, mongoDBURL } from './config.js';
+import itemsRoute from './routes/itemsRoute.js';
+import companyrouter from './routes/companyrouter.js';
 
-const app = express()
+const app = express();
+
 app.use(express.json());
 
-app.use(cors());
+// Enable CORS for all routes
+app.use(cors({
+  origin: 'https://front-kkcb.onrender.com', // Allow requests only from your frontend domain
+  methods: 'GET,POST,PUT,DELETE,OPTIONS', // Specify allowed HTTP methods
+  allowedHeaders: 'Content-Type,Authorization' // Specify allowed headers
+}));
 
-
-app.get('/', (request, response) => {
-    console.log(request);
-    return response.status(234).send('Welcome to Visa Chemical ');
-})
-
-app.use('/items',itemsRoute);
-app.use('/company',companyrouter);
-
-mongoose
-    .connect(mongoDBURL)
-    .then(() => {
-        console.log('App Connected to Database');
-        app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server is running on port ${PORT}`);
+app.get('/', (req, res) => {
+  res.status(200).send('Welcome to Visa Chemical');
 });
-    })
-    .catch((error) => {
-        console.log(error);
-    });
+
+app.use('/items', itemsRoute);
+app.use('/company', companyrouter);
+
+mongoose.connect(mongoDBURL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log('Connected to MongoDB');
+  
+  app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on http://0.0.0.0:${PORT}`);
+  });
+})
+.catch((error) => {
+  console.error('MongoDB connection error:', error);
+});
